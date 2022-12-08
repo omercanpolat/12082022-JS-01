480d00690bb7512fb495dc679161cf8201d7db60
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2022-12-08 22:20:13 +0300

    ders notları

---
+++ intro/app.js
@@ -0,0 +1,4 @@
+console.log("merhaba fs-13");
+console.log("variables and data types");
+
+//* CONST VE LET, ECMASCRIPT6 ile gelmiştir. VAR eski versiyonlardadır.
---
+++ variables-datatypes/app.js
@@ -0,0 +1,14 @@
+console.log("****data types");
+
+const fs = "cohort 13";
+console.log(fs);
+
+const sayi = 20;
+console.log(sayi);
+console.log(typeof sayi);
+
+const sayi1 = "33";
+console.log(sayi1);
+console.log(typeof sayi1);
+
+
