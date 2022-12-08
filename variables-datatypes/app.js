21a36f5f6d180df09d7774f6a074b7714d176f40
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2022-12-08 22:28:38 +0300

    ilk commit

--- variables-datatypes/app.js
+++ variables-datatypes/app.js
@@ -11,4 +11,9 @@ const sayi1 = "33";
 console.log(sayi1);
 console.log(typeof sayi1);
 
+// sayi1 = 55; //!Assignment to constant variable.
 
+const s1 = "hello ";
+const s2 = "FS13";
+console.log(s1 + s2); //?String Concationation
+// JS default olarak + operatöründe eğer değşkenlerden en az birisi string ise yazı olarak bunları birbiri yanına yazar.
