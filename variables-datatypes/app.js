ca84d411b19e2f41f0445a519e10b7a02323c9e5
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2022-12-08 22:48:45 +0300

    ikinci

--- variables-datatypes/app.js
+++ variables-datatypes/app.js
@@ -1,5 +1,5 @@
 console.log("****data types");
-
+//================================== CONST ======================================//
 const fs = "cohort 13";
 console.log(fs);
 
@@ -17,3 +17,35 @@ const s1 = "hello ";
 const s2 = "FS13";
 console.log(s1 + s2); //?String Concationation
 // JS default olarak + operatöründe eğer değşkenlerden en az birisi string ise yazı olarak bunları birbiri yanına yazar.
+
+//================================== LET ====================================//
+
+let dil = "javascript";
+console.log(dil);
+
+dil = "java";
+console.log(dil);
+
+dil = true;
+console.log(dil, typeof dil);
+
+dil = 3.14;
+console.log(dil, typeof dil);
+
+let cohort;
+console.log(cohort); //*undefined (tanımsız)
+
+let num1 = 5;
+let num2 = 3;
+let result = num1 + num2;
+console.log(result); //?8
+
+
+//let num1 = 5;
+//let num2 = 3;
+//let result = num1 + num2;
+//console.log(++result); //?9
+
+
+
+
